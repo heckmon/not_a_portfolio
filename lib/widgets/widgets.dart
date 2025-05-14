6689fb8280b5509feb8857f5c5300f013879309e
ATHUL A S <athulas2005@gmail.com>
2025-05-14 22:48:03 +0530

    Refactor Projects widget to use animation controllers and improve projectTile structure

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -140,50 +140,75 @@ class Projects extends StatefulWidget {
   State<Projects> createState() => _ProjectsState();
 }
 
-class _ProjectsState extends State<Projects> with SingleTickerProviderStateMixin{
-  late Animation<Alignment> _aniAlign;
-  late AnimationController _animationController;
+class _ProjectsState extends State<Projects> with TickerProviderStateMixin{
+
+  late final List<AnimationController> _projectControllers;
+  late final List<Animation<Color?>> _colorAnimations;
 
   @override
   void initState() {
-    _animationController = AnimationController(
-        duration: const Duration(seconds: 1),
-        vsync: this
+     _projectControllers = List.generate(4, (_) => AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: 700),
+      ));
+
+      _colorAnimations = List.generate(4, (index) =>
+        ColorTween(begin: Colors.transparent, end: Colors.greenAccent)
+          .animate(_projectControllers[index])
       );
-    _aniAlign = TweenSequence<Alignment>([
-      TweenSequenceItem(
-        tween: Tween<Alignment>(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter
-        ),
-        weight: 1
-      )
-    ]).animate(_animationController);
+
+      for (int i = 0; i < _projectControllers.length; i++) {
+        Future.delayed(Duration(milliseconds: i * 1500), () {
+          _projectControllers[i].forward();
+        });
+      }
     super.initState();
   }
 
   @override
+  void dispose() {
+    for (int i = 0; i < _projectControllers.length; i++) {
+        _projectControllers[i].dispose();
+      }
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animationController,
-      builder: (context,_) {
-        return ListView(
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: projectTile(
+                _colorAnimations[0],
+                _projectControllers[0],
+                "Seven Segment Display module",
+                "A MicroPython library to control traditional 4 digit seven segment display.",
+                true,
+                "",
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SvgPicture.asset(
+                    width: 45,
+                    "assets/images/python.svg",
+                  ),
+                )
+              ),
+            ),
+            Positioned(
+              left: 47,
+              top: 85,
+              child: Saber(startAfter: Duration(milliseconds: 700), height: 85)
+            ),    
+          ],
+        ),
+        Stack(
           children: [
             projectTile(
-              "Seven Segment Display module",
-              "A MicroPython library to control traditional 4 digit seven segment display.",
-              true,
-              "",
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(
-                  width: 45,
-                  "assets/images/python.svg",
-                ),
-              )
-            ),
-            //TODO: Add Vertical divider
-            projectTile(
+              _colorAnimations[1],
+              _projectControllers[1],
               "AI Whatsapp Bot",
               "A whatsapp bot created by integrating Google Gemini API with Whatsapp API.",
               true,
@@ -196,7 +221,18 @@ class _ProjectsState extends State<Projects> with SingleTickerProviderStateMixin
                 ),
               )
             ),
+            Positioned(
+              left: 47,
+              top: 70,
+              child: Saber(startAfter: Duration(milliseconds: 2200), height: 85)
+            ), 
+          ],
+        ),
+        Stack(
+          children: [
             projectTile(
+              _colorAnimations[2],
+              _projectControllers[2],
               "KTU result prank",
               "KTU result page clone with fake result, I learned flutter by doing this.",
               true,
@@ -212,97 +248,183 @@ class _ProjectsState extends State<Projects> with SingleTickerProviderStateMixin
                 ),
               )
             ),
-            projectTile(
-              "File Tree View",
-              "A flutter package to display directiories in a VScode style tree structure.",
-              true,
-              "",
-              Padding(
-                padding: const EdgeInsets.only(top: 8, bottom:8, right: 11),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SvgPicture.asset(
-                    height: 35,
-                    "assets/images/flutter.svg",
-                  ),
-                ),
-              )
-            ),
+            Positioned(
+              left: 47,
+              top: 75,
+              child: Saber(startAfter: Duration(milliseconds: 3700), height: 85)
+            ), 
           ],
-        );
-      }
+        ),
+        projectTile(
+          _colorAnimations[3],
+          _projectControllers[3],
+          "File Tree View",
+          "A flutter package to display directiories in a VScode style tree structure.",
+          true,
+          "",
+          Padding(
+            padding: const EdgeInsets.only(top: 8, bottom:8, right: 11),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                height: 35,
+                "assets/images/flutter.svg",
+              ),
+            ),
+          )
+        ),
+      ],
     );
   }
 }
 
-Widget projectTile(String title, String subtitle, bool status, String link, dynamic icon){
-  return ListTile(
-    titleTextStyle: GoogleFonts.museoModerno(
-      color: Colors.white,
-      fontSize: 22
-    ),
-    title: Row(
-      spacing: 20,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 15),
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  blurStyle: BlurStyle.outer,
-                  color: Colors.greenAccent.withAlpha(122),
-                  blurRadius: 15,
-                  spreadRadius: 1
-                )
-              ],
-              border: Border.all(
-                color: Colors.greenAccent,
-                width: 1.5
-              ),
-              shape: BoxShape.circle
-            ),
-            child: icon,
-          ),
+Widget projectTile(
+    Animation<Color?> colorAnimation,
+    AnimationController controller,
+    String title,
+    String subtitle,
+    bool status,
+    String link,
+    dynamic icon
+  ){
+  return AnimatedBuilder(
+    animation: controller,
+    builder: (context, child) {
+      return ListTile(
+        titleTextStyle: GoogleFonts.museoModerno(
+          color: Colors.white,
+          fontSize: 22
         ),
-        Text(title)
-      ],
-    ),
-    subtitle: Padding(
-      padding: const EdgeInsets.only(left: 70),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            subtitle,
-            style:  GoogleFonts.montserrat(
-              color: Colors.grey[400],
-              fontSize: 16
+        title: Row(
+          spacing: 20,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 0),
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurStyle: BlurStyle.outer,
+                      color: colorAnimation.value!.withAlpha(122),
+                      blurRadius: 15,
+                      spreadRadius: 1
+                    )
+                  ],
+                  border: Border.all(
+                    color: colorAnimation.value ?? Colors.transparent,
+                    width: 1.5
+                  ),
+                  shape: BoxShape.circle
+                ),
+                child: icon,
+              ),
             ),
-          ),
-          Row(
-            spacing: 5,
+            Text(title)
+          ],
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(left: 70),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "Status: ${status ? "Completed" : "Under development"}",
-                style: GoogleFonts.montserrat(
-                  color: Colors.grey,
-                  fontSize: 14
+                subtitle,
+                style:  GoogleFonts.montserrat(
+                  color: Colors.grey[400],
+                  fontSize: 16
                 ),
               ),
-              Icon(
-                Icons.check_circle_rounded,
-                size: 15,
-                color: Colors.greenAccent,
+              Row(
+                spacing: 5,
+                children: [
+                  Text(
+                    "Status: ${status ? "Completed" : "Under development"}",
+                    style: GoogleFonts.montserrat(
+                      color: Colors.grey,
+                      fontSize: 14
+                    ),
+                  ),
+                  Icon(
+                    Icons.check_circle_rounded,
+                    size: 15,
+                    color: Colors.greenAccent,
+                  )
+                ],
+              ),
+              TextButton(
+                onPressed: (){},
+                child: Text("Source code")
               )
             ],
           ),
-          TextButton(
-            onPressed: (){},
-            child: Text("Source code")
-          )
-        ],
-      ),
-    ),
+        ),
+      );
+    }
   );
+}
+
+
+class Saber extends StatefulWidget {
+  final Duration startAfter;
+  final double? height;
+
+  const Saber({
+    super.key, 
+    required this.startAfter,
+    this.height
+  });
+
+  @override
+  State<Saber> createState() => _SaberState();
+}
+
+class _SaberState extends State<Saber> with SingleTickerProviderStateMixin {
+  
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  
+  @override
+  void initState(){
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 800),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: widget.height ?? 300).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+    Future.delayed(widget.startAfter, ()=> _controller.forward());
+    super.initState();
+  }
+  
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: 2.5,
+            height: _animation.value,
+            decoration: BoxDecoration(
+              color: Colors.greenAccent,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.greenAccent.withAlpha(122),
+                  blurRadius: 8,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
