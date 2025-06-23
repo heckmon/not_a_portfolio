cd9fe77de39eed2c472c49fae0c7f815e395bf6f
ATHUL A S <athulas2005@gmail.com>
2025-06-23 21:39:06 +0530

    feat: Adjust Saber widget height for improved mobile layout

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -351,7 +351,7 @@ class _ProjectsState extends State<Projects> with TickerProviderStateMixin{
             Positioned(
               left: 47,
               top: 75,
-              child: Saber(startAfter: Duration(milliseconds: 3800), height: isMobile ? 110 : 85)
+              child: Saber(startAfter: Duration(milliseconds: 3800), height: isMobile ? 145 : 85)
             ),
           ],
         ),
