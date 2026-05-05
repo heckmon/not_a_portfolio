a985ae480abd2283fe0a90f8b9295160b125fe09
ATHUL A S <64469847+heckmon@users.noreply.github.com>
2026-05-05 21:08:11 +0530

    feat: add Roxum IDE project tile and update animation controllers

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -165,24 +165,24 @@ class _ProjectsState extends State<Projects> with TickerProviderStateMixin{
 
   @override
   void initState() {
-     _projectAnimationControllers = List.generate(5, (_) => AnimationController(
+     _projectAnimationControllers = List.generate(6, (_) => AnimationController(
         vsync: this,
         duration: Duration(milliseconds: 600),
       ));
 
-      _colorAnimations = List.generate(5, (index) =>
+      _colorAnimations = List.generate(6, (index) =>
         ColorTween(begin: Colors.transparent, end: Colors.greenAccent).animate(_projectAnimationControllers[index])
       );
 
-      _textColorAnimation = List.generate(5, (index) => 
+      _textColorAnimation = List.generate(6, (index) => 
         ColorTween(begin: const Color.fromARGB(255, 59, 58, 58), end: Colors.white).animate(_projectAnimationControllers[index])
       );
 
-      _iconColorAnimation = List.generate(5, (index) => 
+      _iconColorAnimation = List.generate(6, (index) => 
         ColorTween(begin: Colors.black, end: Colors.transparent).animate(_projectAnimationControllers[index])
       );
 
-      _subtitleColorAnimation = List.generate(5, (index) => 
+      _subtitleColorAnimation = List.generate(6, (index) => 
         ColorTween(begin: const Color.fromARGB(255, 52, 52, 52), end: Colors.grey[400]).animate(_projectAnimationControllers[index])
       );
 
@@ -386,10 +386,42 @@ class _ProjectsState extends State<Projects> with TickerProviderStateMixin{
             Positioned(
               left: 47,
               top: 75,
-              child: Saber(startAfter: Duration(milliseconds: 4700), height: isMobile ? 110 : 50)
+              child: Saber(startAfter: Duration(milliseconds: 4700), height: isMobile ? 145 : 85)
             ),
           ],
         ),
+        Stack(
+          children: [
+            projectTile(
+              context,
+              _projectAnimationControllers[5],
+              _colorAnimations[5],
+              _textColorAnimation[5],
+              _subtitleColorAnimation[5],
+              "Roxum IDE",
+              "A full fledged IDE on android uses Code Forge under the hood.",
+              Padding(
+                padding: const EdgeInsets.only(top: 8, bottom:8, right: 11),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AnimatedBuilder(
+                    animation: _projectAnimationControllers[4],
+                    builder: (context,_) {
+                      return SvgPicture.asset(
+                        colorFilter: ColorFilter.mode(_iconColorAnimation[4].value!, BlendMode.color),
+                        height: 35,
+                        "assets/images/flutter.svg",
+                      );
+                    }
+                  ),
+                ),
+              ),
+              () async{
+                await launchUrl(Uri.parse("https://github.com/heckmon/roxum-ide"));
+              }
+            ),
+          ],
+        )
       ],
     );
   }
@@ -403,21 +435,6 @@ class PendingProjects extends StatelessWidget {
     bool isMobile = ResponsiveBreakpoints.of(context).isMobile;
     return Column(
       children: [
-        pendingProjectTile(
-          "VSDroid",
-          "A full fledged IDE on android uses Code Forge under the hood.",
-          Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 8, right: 11),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                colorFilter: ColorFilter.mode(Colors.black, BlendMode.color),
-                height: 35,
-                "assets/images/flutter.svg"
-              ),
-            ),
-          )
-        ),
         pendingProjectTile(
           "OS from Scratch",
           "A kernel from scratch using C.",
